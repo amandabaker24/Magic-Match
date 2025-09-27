1cd1e5f73c2c6fef83507739c8beb97a05580662
Amanda Baker <amandarbaker24@gmail.com>
2025-09-27 00:03:02 -0400

    Add files via upload

---
+++ src/components/SingleCard.js
@@ -0,0 +1,22 @@
+import './SingleCard.css'
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }){
+  const handleCLick = () => {
+    if (!disabled) {
+      handleChoice(card)
+    }
+  }
+return (
+   <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+          <img className="front" src={card.src} alt="card front" />
+          <img 
+            className="back" 
+            src="/img/background.png" 
+            onClick={handleCLick}
+            alt="card back" 
+          />
+        </div>
+      </div>
+    )
+}
